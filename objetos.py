43a42531adcdf5124f81647f39bf5860ffce209a
juanjoudesa <141250002+juanjoudesa@users.noreply.github.com>
2023-11-17 18:21:08 -0300

    Add files via upload

--- objetos.py
+++ objetos.py
@@ -1,54 +1,55 @@
-class vehiculo:
-    def __init__(self, name, forma, vida):
-        self.name = name 
-        self.forma = forma
+class Vehiculo:
+    def __init__(self, nombre, largo, ancho, alto, vida, cant):
+        self.nombre = nombre
+        self.largo = largo
+        self.ancho = ancho
+        self.alto = alto
         self.vida = vida
-  
-class globo(vehiculo):
-    def __init__(self, name, forma, vida):
-        super().__init__(name, forma, vida)
-        self.name = name
-        self.forma = (3,3,3)
-        self.vida = 1
+        self.cant = cant
 
-class zeppelin(vehiculo):
-    def __init__(self, name, forma, vida):
-        super().__init__(name, forma, vida)
-        self.name = name
-        self.forma = (5,2,2)
-        self.vida = 3
+    def rotar(self):
+        self.ancho, self.largo = self.largo, self.ancho
+        return self.ancho, self.largo
 
-class avion:
-    def __init__(self, name, forma, vida):
-        super().__init__(name, forma, vida)
-        pass
+    def recibir_disparo(self):
+        self.vida -= 1
+        return self
+    
+    def crear_vehiculo(self, nombre_p):
+        try:
+            cantidad = self.cant  # Obtiene la cantidad desde el objeto Vehiculo correspondiente
+            for i in range(1, cantidad + 1):
+                while True:
+                    try:
+                        posicion = tuple(map(int, input(f'Ingrese la posición del {nombre_p}: ').split())) 
+                        break
+                    except ValueError:
+                        print("Error: Ingrese números válidos para la posición.")
 
-class elevador:
-    def __init__(self, name, forma, vida):
-        super().__init__(name, forma, vida)
-        self.name = name
-        self.forma = (1,1,10)
-        self.vida = 4
-
-class mapa:
-    def __init__(self) -> None:
-        self.x = 15
-        self.y = 15
-        self.z = 10
-#definir el objeto en el codigo principal
+                return posicion
+        except AttributeError:
+            raise NotImplementedError("Cantidad no definida para este tipo de vehículo")
 
 
+class Globo(Vehiculo):
+    def __init__(self, nombre):
+        super().__init__("globo", 3, 3, 3, 1, 5)
+        
+
+class Zepellin(Vehiculo):
+    def __init__(self, nombre):
+        super().__init__("zepellin", 5, 2, 2, 3, 2)
+        
+
+class Avion(Vehiculo):
+    def __init__(self, nombre):
+        super().__init__("avion", 4, 3, 2, 2, 3)
+        
+
+class Elevador(Vehiculo):
+    def __init__(self, nombre):
+        super().__init__("elevador", 1, 1, 10, 4, 1)
+        
 
     
 
-
-
-
-
-
-
-
-            
-    
-
-    
